cd0bf7717b555cc5b9b3849a8d990a93915e6716
a16criferart <49525733+a16criferart@users.noreply.github.com>
2019-04-26 16:28:37 +0200

    Primer commit
    Creación de servidor node.js y webs de prueba ( falta estructurar)

---
+++ server.js
@@ -0,0 +1,25 @@
+console.log("Hola, iniciando");
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+console.log("Vamos a cargar las páginas:");
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/jugar',function(req,res){
+  res.sendFile(path.join(__dirname+'/jugar.php'));
+});
+
+router.get('/perfil',function(req,res){
+  res.sendFile(path.join(__dirname+'/perfil.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log("ok");
+console.log('Running at Port 3000');
