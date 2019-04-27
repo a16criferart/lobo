5c72c94c22cc86724750e9b914c51f8d527593ff
Cristina <a16criferart@iam.cat>
2019-04-28 00:12:43 +0200

    Script ciclo

--- public/js/jugar.js
+++ public/js/jugar.js
@@ -1,1 +1,21 @@
-console.log("Hola");
+window.onload = function() {
+    ciclo();
+  };
+
+function ciclo() {
+    console.log("La partida se iniciara en 15 segundos");
+    setInterval( fase1(), 15000);
+}
+
+function fase1() {
+    console.log ("FASE 1");
+    setTimeout(fase2(), 5000);
+}
+function fase2() {
+    console.log ("FASE 2");
+    setTimeout(fase3(), 5000);
+}
+function fase3() {
+    console.log ("FASE 3");
+    setTimeout(console.log("CICLO COMPLETADO"), 5000);
+}
