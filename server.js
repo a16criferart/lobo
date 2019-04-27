3130ea28c15f82ef47602ecfe5c868399a513f44
Cristina <a16criferart@iam.cat>
2019-04-27 17:26:53 +0200

    Autentific. de users y nueva estructura

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
-console.log("Hola, iniciando");
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+// server.js
 
-console.log("Vamos a cargar las páginas:");
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-router.get('/jugar',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/jugar.php'));
-});
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-router.get('/perfil',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/perfil.html'));
-});
+var configDB = require('./config/database.js');
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-console.log("ok");
-console.log('Running at Port 3000');
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
