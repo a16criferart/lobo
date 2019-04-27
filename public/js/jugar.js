2795804af1ea8ea9dba2f16f416a4519a74d2359
Cristina <a16criferart@iam.cat>
2019-04-28 00:31:30 +0200

    Script ciclo+counter

--- public/js/jugar.js
+++ public/js/jugar.js
@@ -19,3 +19,28 @@ function fase3() {
     console.log ("FASE 3");
     setTimeout(console.log("CICLO COMPLETADO"), 5000);
 }
+function contador() {
+
+    var counter = 5;
+    var interval = setInterval(function() {
+    counter--;
+    console.log(counter)
+    if (counter == 0) {
+        // Display message
+        clearInterval(interval);
+    }
+}, 1000);
+}
+
+function pre_contador() {
+
+var counter = 15;
+var interval = setInterval(function() {
+counter--;
+console.log(counter)
+if (counter == 0) {
+    // Display message
+    clearInterval(interval);
+    }
+}, 1000);
+}
